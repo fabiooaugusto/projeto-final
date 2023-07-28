56787b28308d2f2cc9b703f3edc92d998806741b
Andre <meloluizandre10@gmail.com>
2023-07-28 17:27:33 -0300

    feat(new page): create recipe book page

--- app/api/recipes.ts
+++ app/api/recipes.ts
@@ -1,21 +1,24 @@
 interface Recipe {
-    id: number
-    title: string
-    ingredients: string[]
-    imageUrl: string
+	id: number
+	title: string
+	ingredients: string[]
+	imageUrl: string
+	category: number
 }
 
 export const recipes: Recipe[] = [
-    {
-        id: 0,
-        title: 'bacon & eggs',
-        ingredients: ['bacon', 'eggs'],
-        imageUrl: ''
-    },
-    {
-        id: 1,
-        title: 'fodase',
-        ingredients: [],
-        imageUrl: ''
-    }
+	{
+		id: 0,
+		title: 'bacon & eggs',
+		ingredients: ['bacon', 'eggs'],
+		imageUrl: '',
+		category: 0,
+	},
+	{
+		id: 1,
+		title: '',
+		ingredients: [],
+		imageUrl: '',
+		category: 0,
+	},
 ]
--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
-import { recipes } from "../recipes";
+import { NextResponse } from 'next/server'
+import { recipes } from '../recipes'
 
 export async function GET(request: Request) {
-    return NextResponse.json(recipes)
+	return NextResponse.json(recipes)
 }
