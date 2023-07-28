5ec2ed298da8f8e10cd309cb3bda9d1e1acdb785
Andre <meloluizandre10@gmail.com>
2023-07-28 01:40:32 -0300

    feat(api): create basic API

---
+++ app/api/recipes/[recipeId]/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { recipes } from "../../recipes";
+
+export async function GET(request: Request, {params}: {params: {recipeId: string}}) {
+    const recipe = recipes.find((recipe) => recipe.id === Number(params.recipeId))
+
+    console.log(recipe, params.recipeId, recipes)
+
+    return NextResponse.json(recipe || 'no recipe found') 
+}
---
+++ app/api/recipes/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import { recipes } from "../recipes";
+
+export async function GET(request: Request) {
+    return NextResponse.json(recipes)
+}
